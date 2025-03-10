88374d3d4c843b76ebef2216782c8e281a68e0ad
tsema.g <tsema.g@medlinx.online>
2025-03-10 23:24:15 +0300

    Signed-off-by: tsema.g <tsema.g@medlinx.online>

--- js/randomStringUtils.js
+++ js/randomStringUtils.js
@@ -3,6 +3,10 @@ export class RandomStringUtils {
 	static randomByTemplate(template) {
 		if (template === '%snils') return this._generateSNILS();
 		
+		if (template === '%date') {
+			return this._generateDate();
+		}
+		
 		const regex = /(%[dsm][1-9]\d?)/g;
 		let result = '';
 		let endIdx = 0;
@@ -60,5 +64,12 @@ export class RandomStringUtils {
 		let checksum = snils.split('').reduce((sum, digit, index) => sum + digit * (9 - index), 0) % 101;
 		if (checksum > 99) checksum = 0;
 		return `${snils.substr(0, 3)}-${snils.substr(3, 3)}-${snils.substr(6, 3)} ${checksum.toString().padStart(2, '0')}`;
+	}
+	
+	static _generateDate() {
+		const randomDay = String(Math.floor(Math.random() * 28) + 1).padStart(2, "0");
+		const randomMonth = String(Math.floor(Math.random() * 12) + 1).padStart(2, "0");
+		const randomYear = String(Math.floor(Math.random() * 30) + 1990);
+		return `${randomDay}.${randomMonth}.${randomYear}`;
 	}
 }
